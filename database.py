2badccf8610c798e3b9984a1f588a8625b824703
LohithVSV <lohith.virothi.sv@gmail.com>
2026-05-15 18:14:29 +0530

    new supabase modification

--- database.py
+++ database.py
@@ -1,16 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from dotenv import load_dotenv
 import os
 
-load_dotenv()   #loads environment variables from .env file
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-DATABASE_URL=os.environ.get("DATABASE_URL")       #connects to database
-
-engine=create_engine(DATABASE_URL)   #creates a database engine
-
-Base=declarative_base() 
-
+engine = create_engine(DATABASE_URL)
+Base = declarative_base()
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 def get_db():
